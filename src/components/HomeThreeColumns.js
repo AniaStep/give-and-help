4af0f81cc119558db6a570acbf250f4b4ab8761b
Anna Stępień <147930506+AniaStep@users.noreply.github.com>
2024-05-27 17:55:38 +0200

    Added three columns section to home page

--- src/components/HomeThreeColumns.js
+++ src/components/HomeThreeColumns.js
@@ -2,6 +2,24 @@ import React from 'react';
 
 export const HomeThreeColumns = () => {
     return (
-        <div id="what-it-is-about"></div>
+        <div id="what-it-is-about">
+            <div className="stats-column">
+                <p className="stats-num">10</p>
+                <p className="stats-num-desc">oddanych worków</p>
+                <p className="stats-full-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab dolor fugiat magni nemo placeat ut voluptas.</p>
+            </div>
+            <div className="stats-column">
+                <p className="stats-num">5</p>
+                <p className="stats-num-desc">wspartych organizacji</p>
+                <p className="stats-full-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab dolor fugiat magni nemo placeat ut
+                    voluptas.</p>
+            </div>
+            <div className="stats-column">
+                <p className="stats-num">7</p>
+                <p className="stats-num-desc">zorganizowanych zbiórek</p>
+                <p className="stats-full-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab dolor fugiat magni nemo placeat ut
+                    voluptas.</p>
+            </div>
+        </div>
     )
 }
