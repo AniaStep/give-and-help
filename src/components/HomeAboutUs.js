8dc5bf55831d724066df7326f124f90d94a1ea8a
Anna Stępień <147930506+AniaStep@users.noreply.github.com>
2024-05-30 18:23:00 +0200

    Added About Us component to Home page

--- src/components/HomeAboutUs.js
+++ src/components/HomeAboutUs.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import decorationImage from "../assets/decoration.svg";
+
 
 export const HomeAboutUs = () => {
     return (
-        <div id="about-us"></div>
+        <div id="about-us">
+            <div className="about-us-text">
+                <p>O nas</p>
+                <img src={decorationImage} alt=""/>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet consequatur id, natus nihil pariatur reprehenderit. Aliquam architecto aut dolor nulla obcaecati quaerat, quam quas quasi!</p>
+            </div>
+            <div className="about-us-image"></div>
+        </div>
     )
 }
