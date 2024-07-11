ca94f7d78fc3fdf8805763553d7f556f63d5cc52
Anna Stępień <147930506+AniaStep@users.noreply.github.com>
2024-07-11 21:51:25 +0200

    Fixed navigation

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,31 +1,37 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-import { Link as ScrollLink } from "react-scroll";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
 import decorationImage from "../assets/decoration.svg";
-import { useLocation } from "react-router-dom";
-
 
 export const Header = () => {
     const location = useLocation();
-
+    const navigate = useNavigate();
     const pathName = location.pathname.split("/")[1];
+
+    const handleNavigation = (to) => {
+        navigate("/");
+        setTimeout(() => {
+            scroll.scrollTo(document.getElementById(to).offsetTop);
+        }, 0);
+    };
+
     return (
         <>
             <div className="home-hero-header">
                 <div className="top-header">
-                    <Link className={pathName === "logowanie" && "active"} to="/logowanie">Zaloguj</Link>
-                    <Link className={pathName === "rejestracja" && "active"} to="/rejestracja">Załóż konto</Link>
+                    <Link className={pathName === "logowanie" ? "active" : undefined} to="/logowanie">Zaloguj</Link>
+                    <Link className={pathName === "rejestracja" ? "active" : undefined} to="/rejestracja">Załóż konto</Link>
                 </div>
                 <div className="bottom-header">
-                    <ScrollLink to="start"><Link to="/">Start</Link></ScrollLink>
-                    <ScrollLink to="what-it-is-about"><Link to="/">O co chodzi?</Link></ScrollLink>
-                    <ScrollLink to="about-us"><Link to="/">O nas</Link></ScrollLink>
-                    <ScrollLink to="organizations"><Link to="/">Fundacja i organizacje</Link></ScrollLink>
-                    <ScrollLink to="contact"><Link to="/">Kontakt</Link></ScrollLink>
+                    <span onClick={() => handleNavigation("start")}>Start</span>
+                    <span onClick={() => handleNavigation("what-it-is-about")}>O co chodzi?</span>
+                    <span onClick={() => handleNavigation("about-us")}>O nas</span>
+                    <span onClick={() => handleNavigation("organizations")}>Fundacja i organizacje</span>
+                    <span onClick={() => handleNavigation("contact")}>Kontakt</span>
                 </div>
             </div>
         </>
-    )
+    );
 }
 
 export const HomeHeader = () => {
@@ -33,17 +39,17 @@ export const HomeHeader = () => {
         <div id="start">
             <div className="home-hero-image"></div>
             <div className="home-hero-content">
-               <Header/>
+                <Header/>
                 <div className="home-hero-text">
                     <p>Zacznij pomagać!</p>
                     <p>Oddaj niechciane rzeczy w zaufane ręce</p>
                     <img src={decorationImage} alt=""/>
                     <div className="home-hero-buttons">
-                        <button><Link to="logowanie">Oddaj rzeczy</Link></button>
-                        <button><Link to="logowanie">Zorganizuj zbiórkę</Link></button>
+                        <button><Link to="/logowanie">Oddaj rzeczy</Link></button>
+                        <button><Link to="/logowanie">Zorganizuj zbiórkę</Link></button>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
